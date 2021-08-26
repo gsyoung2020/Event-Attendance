6d391c271cebecb7d892ed9af3a622a82dfe8473
Alton Michaux <alton.michaux@yahoo.com>
2021-08-26 18:17:03 -0400

    <ice_cube>ice_cube gem is now fully functional

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,18 +12,18 @@ class Event < ApplicationRecord
 
   def schedule(start = Time.zone.now.to_date)
     IceCube::Schedule.new(start) do |s|
-      s.add_recurrence_rule IceCube::Rule.daily.until(Date.today + 30) if frequency === 'Daily'
-      s.add_recurrence_rule IceCube::Rule.weekly.until(Date.today + 30) if frequency === 'Weekly'
-      s.add_recurrence_rule IceCube::Rule.monthly.until(Date.today + 30) if frequency === 'Monthly'
-      s.add_recurrence_rule IceCube::Rule.yearly.until(Date.today + 30) if frequency === 'Annually'
+      s.add_recurrence_rule IceCube::Rule.daily.until(Date.today + 3650) if frequency === 'Daily'
+      s.add_recurrence_rule IceCube::Rule.weekly.until(Date.today + 3650) if frequency === 'Weekly'
+      s.add_recurrence_rule IceCube::Rule.monthly.until(Date.today + 3650) if frequency === 'Monthly'
+      s.add_recurrence_rule IceCube::Rule.yearly.until(Date.today + 3650) if frequency === 'Annually'
     end
   end
 
   def calendar_events(start)
     start_date = start.beginning_of_month.beginning_of_week
-    end_date = start.end_of_month.end_of_week
+    end_date = end_time.end_of_month.end_of_week
     schedule(start_date).occurrences(end_date).map do |date|
-      Event.new(id: id, description: description, frequency: frequency, start_time: date, end_time: date)
+      Event.new(id: id, description: description, frequency: frequency, start_time: date)
     end
   end
 end
