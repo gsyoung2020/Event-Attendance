08248e913821c935e6c8246cb862f35d2fb50445
Alton Michaux <alton.michaux@yahoo.com>
2021-08-27 09:02:18 -0400

    <rspec> finished setting up test specs for events routes, will look into specs for calendar views and scheduling next

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,8 +2,8 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
+# RSpec.describe 'Users', type: :request do
+#   describe 'GET /index' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+# end
