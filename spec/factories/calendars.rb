4e67cbccd15cd9bbb85f165f422c3557d51ba964
Alton Michaux <alton.michaux@yahoo.com>
2021-08-27 09:21:12 -0400

    <database>removed unnecessary "start" and "end" columns because event model already has "start_time" and "end_time" columns

---
+++ spec/factories/calendars.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# FactoryBot.define do
+#     factory :calendar do |f|
+#       f.schedule { nil }
+#       f.calendar_events { nil }
+#     end
+#   end
---
+++ spec/requests/calendar_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Calendar', type: :request do
+    describe 'calendar index view' do
+        it 'renders the index view for calendar' do
+
+        end
+    end
+end
