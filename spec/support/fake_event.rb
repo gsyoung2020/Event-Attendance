c5ab0c423eda42d49ace2c5c7ab4a610185b8f17
Alton Michaux <alton.michaux@yahoo.com>
2021-08-28 15:36:58 -0400

    <rspec>started writing model specs for simple_calendar, will eventually figure out how to test controller specs (views)

---
+++ spec/support/fake_event.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class FakeEvent
+  attr_accessor :name, :start_time, :end_time
+
+  def initialize(name = 'event', start_time = nil, end_time = nil)
+    @name = name
+    @start_time = start_time
+    @end_time = end_time
+  end
+end
