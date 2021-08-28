40fe4f2c3d5d32e0ccd63be24844f47949fa6232
Alton Michaux <alton.michaux@yahoo.com>
2021-08-28 12:33:15 -0400

    <rspec>still setting up simple_calendar specs

--- spec/requests/calendar_spec.rb
+++ spec/requests/calendar_spec.rb
@@ -4,17 +4,21 @@ require 'rails_helper'
 require 'simple_calendar'
 
 RSpec.describe SimpleCalendar::Calendar, focus: true do
-  describe 'calendar index view' do
-    it 'has a param that determines the start date of the calendar'
-    it 'generates a default date if no start is present'
-    it 'has a range of dates'
-    it 'can split the range of dates into weeks'
-    it 'has a title'
-    it 'has a next view link'
-    it 'has a previous view link'
-    it 'accepts an array of events'
-    it 'sorts the events'
-    it 'yields the events for each day'
-    it 'doesnt crash when an event has a nil start_time'
+  let(:calendar) { SimpleCalendar::Calendar.new(nil) }
+
+  it 'renders a partial with the same name as its class' do
+    expect(calendar.send(:partial_name)).to eq('../../app/views/events/calendar')
   end
+
+  it 'has a param that determines the start date of the calendar'
+  it 'generates a default date if no start is present'
+  it 'has a range of dates'
+  it 'can split the range of dates into weeks'
+  it 'has a title'
+  it 'has a next view link'
+  it 'has a previous view link'
+  it 'accepts an array of events'
+  it 'sorts the events'
+  it 'yields the events for each day'
+  it 'doesnt crash when an event has a nil start_time'
 end
