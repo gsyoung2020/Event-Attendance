15f9b7021545bd92dfa5f1822894bafc8a765fbf
gsyoung2020 <gsyoung2019@gmail.com>
2021-09-01 16:33:36 -0400

    missing code fixed

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe UserMailer, type: :mailer do
   describe 'Mailer' do
     user = FactoryBot.create(:user)
-    let(:mail) {
+    let(:mail) {UserMailer.new_user_email(user)}
 
     it 'renders the subject' do
       expect(mail.subject).to eq('You got a new order!')
