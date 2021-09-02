bff886bf9e0e2220b052eeb58aa42b25000f68a2
gsyoung2020 <gsyoung2019@gmail.com>
2021-09-01 22:53:57 -0400

    mailer rspec fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
     def new_user_email
         @user = params[:user]
-    
+        
+        
         mail(to: @user.email, subject: "You got a new order!")
       end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe UserMailer, type: :mailer do
   describe 'Mailer' do
     user = FactoryBot.create(:user)
-    let(:mail) {UserMailer.new_user_email(user)}
+    let(:mail) {UserMailer.with(user: user).new_user_email}
 
     it 'renders the subject' do
       expect(mail.subject).to eq('You got a new order!')
     end
 
     it 'renders the receiver email' do
-      expect(mail.to).to eq('eattendace@gmail.com')
+      expect(mail.to).to eq([user.email])
     end
 
     it 'renders the sender email' do
